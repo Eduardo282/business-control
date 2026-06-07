5c0b88456be065afdbe7ddc7e26e50927e60b75e
Eduardo Francisco Garcia <lalito9270@gmail.com>
2026-06-07 15:20:11 -0600

    prueba gitactions 5

--- tests/e2e/product-registration.spec.js
+++ tests/e2e/product-registration.spec.js
@@ -11,13 +11,12 @@ async function loginAsAdmin(page) {
   await page.locator('input[type="email"]').fill(adminEmail);
   await page.locator('input[type="password"]').fill(adminPassword);
 
-  // 1. Hacemos clic en Iniciar Sesión (Sin Promise.all, solo el clic directo)
+ // 1. Hacemos clic en Iniciar Sesión
     await page.getByRole('button', { name: /Iniciar/i }).click();
 
-    // 2. Esperamos a que el sistema procese el login y aparezca algo del "Dashboard" o del Layout principal.
-    // Ajusta esta línea para buscar algo que SIEMPRE aparezca cuando ya estás dentro del sistema.
-    // Por ejemplo, el botón de "Cerrar sesión" o el texto de bienvenida.
-    await expect(page.getByText(/Sistema Empresarial/i)).not.toBeVisible({ timeout: 10000 }); // Espera a que desaparezca la pantalla de login
+    // 2. Esperamos a que el campo de "Correo electrónico" desaparezca. 
+    // Esto es único del Login y no viola el Strict Mode.
+    await expect(page.locator('input[type="email"]')).not.toBeVisible({ timeout: 10000 });
     
     // Opcional: Espera a que un elemento de la nueva pantalla sea visible
     // await expect(page.getByRole('button', { name: /Cerrar sesión/i })).toBeVisible();
